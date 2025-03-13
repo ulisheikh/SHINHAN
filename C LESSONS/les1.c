d193215dfe27ff0cb971d577eb4fbcb517a814d5
ulee <ulee@kali.kali>
2025-03-13 11:45:18 +0900

    upload files

--- C LESSONS/les1.c
+++ C LESSONS/les1.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
 int main() {
+
     int a = 100;
     int b = 200;
     int c = a + b;
+    printf("\n%d + %d = %d",a,b,c);
     
-    printf("%d + %d = %d",a,b,c);
+    int a1 = 5;
+    int b1 = 2;
+    int c1 = a1 * b1;
+    printf("\n%d * %d = %d",a1,b1,c1);    
+    
+    int a2 = 100;
+    int b2 = 2;
+    int c2 = a2 / b2;
+    printf("\n%d / %d = %d",a2,b2,c2);
+
+    double a3 = 100.2;
+    double b3 = 100.5;
+    double c3 = a3+b3;
+    printf("\n%f + %f = %f",a3,b3,c3);
+
 
     return 0;
 }
