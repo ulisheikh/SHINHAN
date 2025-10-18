1af58919995509ea12d853c99e96d050d34495be
ulee <ulee@kali.ulee>
2025-10-18 16:06:54 -0500

    Deleted files

--- example.py
+++
@@ -1,8 +0,0 @@
-try:
-    a = int(input("첫번째 정수를 입력해 주세요: "))  
-    b = int(input("두번째 정수를 입력해 주세요: ")) 
-    natija = a + b
-    print(f"{a} + {b} = {natija}")  
-except ValueError:
-    print("Iltimos, faqat son kiriting!")  
-
